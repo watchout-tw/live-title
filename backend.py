1e1a4b4ac54878cd04db3470167494f66c87c5a3
Charlieeeee <charlieworld2008@gmail.com>
2018-11-09 03:16:55 +0800

    [version] debate

--- backend.py
+++ backend.py
@@ -9,6 +9,7 @@ from instance import error_msg
 import time
 from werkzeug import secure_filename
 import os
+from threading import Timer
 
 TITLE = u"活動即將開始"
 TITLE_COLOR = "#000000"
@@ -22,16 +23,19 @@ DATA_DEBATE = {
     'display': False
 }
 
-DEBATE_ALERT_FOLDER = 'static/dabate/alert'
-DEBATE_Q_FOLDER = 'static/dabate/question'
+DEBATE_ALERT_FOLDER = 'static/debate/alert'
+DEBATE_Q_FOLDER = 'static/debate/question'
+DEBATE_A_FOLDER = 'static/debate/answer'
+DEBATE_C_FOLDER = 'static/debate/comment'
 UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['DEBATE_ALERT_FOLDER'] = DEBATE_ALERT_FOLDER
 app.config['DEBATE_Q_FOLDER'] = DEBATE_Q_FOLDER
+app.config['DEBATE_A_FOLDER'] = DEBATE_A_FOLDER
+app.config['DEBATE_C_FOLDER'] = DEBATE_C_FOLDER
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -58,20 +62,94 @@ def get_q_photo():
 def save_q_photo(file):
     ts = time.time()
     ts = int(ts*1000000)
-    DATA_Q_ALERT['img'] = str(ts) + '.png'
-    file.save(os.path.join(app.config['DEBATE_Q_FOLDER'], DATA_Q_ALERT['img']))
+    DATA_DEBATE_Q['img'] = str(ts) + '.png'
+    file.save(os.path.join(app.config['DEBATE_Q_FOLDER'], DATA_DEBATE_Q['img']))
 
+def get_answer_photo():
+    files = os.listdir(app.config['DEBATE_A_FOLDER'])
+    files.sort()
+    file = files[-1]
+    return (app.config['DEBATE_A_FOLDER']+'/'+file)
+
+def save_answer_photo(file):
+    ts = time.time()
+    ts = int(ts*1000000)
+    DATA_DEBATE_ANSWER['img'] = str(ts) + '.png'
+    file.save(os.path.join(app.config['DEBATE_A_FOLDER'], DATA_DEBATE_ANSWER['img']))
+
+def get_comment_photo():
+    files = os.listdir(app.config['DEBATE_C_FOLDER'])
+    files.sort()
+    file = files[-1]
+    return (app.config['DEBATE_C_FOLDER']+'/'+file)
+
+def save_comment_photo(file):
+    ts = time.time()
+    ts = int(ts*1000000)
+    DATA_DEBATE_COMMENT['img'] = str(ts) + '.png'
+    file.save(os.path.join(app.config['DEBATE_C_FOLDER'], DATA_DEBATE_COMMENT['img']))
 
 DATA_DEBATE_ALERT = {
     'img': get_alert_photo(),
-    'display': False
+    'display': False,
+    'countDown': 4,
+    'currentCountDown': 0
 }
 
-DATA_Q_ALERT = {
+DATA_DEBATE_Q = {
     'title': 'Q_TITLE',
     'img': get_q_photo(),
-    'display': False
+    'display': False,
+    'countDown': 10,
+    'currentCountDown': 0
 }
+
+DATA_DEBATE_ANSWER = {
+    'img': get_answer_photo(),
+    'display': False,
+    'countDown': 15,
+    'currentCountDown': 0
+}
+
+DATA_DEBATE_COMMENT = {
+    'title': 'COMMENT_TITLE',
+    'img': get_comment_photo(),
+    'display': False,
+    'countDown': 6,
+    'currentCountDown': 0
+}
+
+
+def countDown():
+    global DATA_DEBATE_ALERT
+    global DATA_DEBATE_Q
+    global DATA_DEBATE_ANSWER
+    global DATA_DEBATE_COMMENT
+
+    if DATA_DEBATE_ALERT['currentCountDown'] > 0:
+        DATA_DEBATE_ALERT['currentCountDown'] -= 1
+    else:
+        DATA_DEBATE_ALERT['display'] = False
+    
+    if DATA_DEBATE_Q['currentCountDown'] > 0:
+        DATA_DEBATE_Q['currentCountDown'] -= 1
+    else:
+        DATA_DEBATE_Q['display'] = False
+    
+    if DATA_DEBATE_ANSWER['currentCountDown'] > 0:
+        DATA_DEBATE_ANSWER['currentCountDown'] -= 1
+    else:
+        DATA_DEBATE_ANSWER['display'] = False
+    
+    if DATA_DEBATE_COMMENT['currentCountDown'] > 0:
+        DATA_DEBATE_COMMENT['currentCountDown'] -= 1
+    else:
+        DATA_DEBATE_COMMENT['display'] = False
+    # print str(DATA_DEBATE_ALERT['currentCountDown']) + ' | ' + str(DATA_DEBATE_Q['currentCountDown'] + ' | ' + str(DATA_DEBATE_ANSWER['currentCountDown'])
+    t = Timer(1.0, countDown)
+    t.start()
+
+
 
 @app.route('/')
 def index():
@@ -90,20 +168,26 @@ def test_back():
     return render_template('debate_back.html')
 
 
-@app.route('/debate/title', methods=['GET', 'POST'])
+@app.route('/debate/data', methods=['GET'])
 def test():
-    global TITLE
-    global TITLE_COLOR
-    global TITLE_SIZE
-    if request.method == 'POST':
-        data = request.get_json(silent=True)
-        DATA_DEBATE['title'] = unicode(data['title'])
-        DATA_DEBATE['color'] = unicode(data['color'])
-        DATA_DEBATE['size'] = data['size']
-        DATA_DEBATE['display'] = unicode(data['display'])
-        return generate_json(DATA_DEBATE)
+    global DATA_DEBATE_ALERT
+    global DATA_DEBATE_Q
+    global DATA_DEBATE_ANSWER
+    global DATA_DEBATE_COMMENT
+    if request.method == 'GET':
+        DATA_DEBATE_ALERT['img'] = get_alert_photo()
+        DATA_DEBATE_Q['img'] = get_q_photo()
+        DATA_DEBATE_ANSWER['img'] = get_answer_photo()
+        DATA_DEBATE_COMMENT['img'] = get_comment_photo()
+        outputData = {
+            'debateAlert': DATA_DEBATE_ALERT,
+            'debateQuestion': DATA_DEBATE_Q,
+            'debateAnswer': DATA_DEBATE_ANSWER,
+            'debateComment': DATA_DEBATE_COMMENT
+        }
+        return generate_json(outputData)
     else:
-        return generate_json(DATA_DEBATE)
+        return generate_json({})
 
 @app.route('/debate/data/alert', methods=['GET', 'POST'])
 def debate_alert():
@@ -112,6 +196,8 @@ def debate_alert():
         data = request.get_json(silent=True)
         DATA_DEBATE_ALERT['display'] = data['display']
         DATA_DEBATE_ALERT['img'] = get_alert_photo()
+        DATA_DEBATE_ALERT['countDown'] = int(data['countDown'])
+        DATA_DEBATE_ALERT['currentCountDown'] = int(data['currentCountDown'])
         return generate_json(DATA_DEBATE_ALERT)
     else:
         DATA_DEBATE_ALERT['img'] = get_alert_photo()
@@ -131,28 +217,98 @@ def debate_alert_photo():
 
 @app.route('/debate/data/question', methods=['GET', 'POST'])
 def debate_question():
-    global DATA_Q_ALERT
+    global DATA_DEBATE_ALERT
+    global DATA_DEBATE_Q
+    global DATA_DEBATE_ANSWER
     if request.method == 'POST':
         data = request.get_json(silent=True)
-        DATA_Q_ALERT['display'] = data['display']
-        DATA_Q_ALERT['img'] = get_q_photo()
-        return generate_json(DATA_Q_ALERT)
+        if DATA_DEBATE_ANSWER['display'] is False: 
+            DATA_DEBATE_Q['display'] = data['display']
+        DATA_DEBATE_Q['img'] = get_q_photo()
+        DATA_DEBATE_Q['title'] = data['title']
+        DATA_DEBATE_Q['countDown'] = int(data['countDown'])
+        DATA_DEBATE_Q['currentCountDown'] = int(data['currentCountDown'])
+        return generate_json(DATA_DEBATE_Q)
     else:
-        DATA_Q_ALERT['img'] = get_q_photo()
-        return generate_json(DATA_Q_ALERT)
+        DATA_DEBATE_Q['img'] = get_q_photo()
+        return generate_json(DATA_DEBATE_Q)
 
 @app.route('/debate/data/question/photo', methods=['GET', 'POST'])
 def debate_question_photo():
-    global DATA_Q_ALERT
+    global DATA_DEBATE_Q
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
             save_q_photo(file)
-            return generate_json(DATA_Q_ALERT)
+            return generate_json(DATA_DEBATE_Q)
     else:
-        DATA_Q_ALERT['img'] = get_q_photo()
-        return generate_json(DATA_Q_ALERT)
+        DATA_DEBATE_Q['img'] = get_q_photo()
+        return generate_json(DATA_DEBATE_Q)
 
+@app.route('/debate/data/answer', methods=['GET', 'POST'])
+def debate_answer():
+    global DATA_DEBATE_ALERT
+    global DATA_DEBATE_Q
+    global DATA_DEBATE_ANSWER
+    if request.method == 'POST':
+        data = request.get_json(silent=True)
+        DATA_DEBATE_ANSWER['display'] = data['display']
+        if data['display']:
+            DATA_DEBATE_ALERT['display'] = False
+            DATA_DEBATE_Q['display'] = False
+        DATA_DEBATE_ANSWER['img'] = get_answer_photo()
+        DATA_DEBATE_ANSWER['countDown'] = int(data['countDown'])
+        DATA_DEBATE_ANSWER['currentCountDown'] = int(data['currentCountDown'])
+        return generate_json(DATA_DEBATE_ALERT)
+    else:
+        DATA_DEBATE_ANSWER['img'] = get_answer_photo()
+        return generate_json(DATA_DEBATE_ANSWER)
+
+@app.route('/debate/data/answer/photo', methods=['GET', 'POST'])
+def debate_answer_photo():
+    global DATA_DEBATE_ANSWER
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            save_answer_photo(file)
+            return generate_json(DATA_DEBATE_ANSWER)
+    else:
+        DATA_DEBATE_ANSWER['img'] = get_answer_photo()
+        return generate_json(DATA_DEBATE_ANSWER)
+
+@app.route('/debate/data/comment', methods=['GET', 'POST'])
+def debate_comment():
+    global DATA_DEBATE_ALERT
+    global DATA_DEBATE_Q
+    global DATA_DEBATE_ANSWER
+    global DATA_DEBATE_COMMENT
+    if request.method == 'POST':
+        data = request.get_json(silent=True)
+        DATA_DEBATE_COMMENT['display'] = data['display']
+        if data['display']:
+            DATA_DEBATE_ALERT['display'] = False
+            DATA_DEBATE_Q['display'] = False
+            DATA_DEBATE_ANSWER['display'] = False
+        DATA_DEBATE_COMMENT['img'] = get_comment_photo()
+        DATA_DEBATE_COMMENT['title'] = data['title']
+        DATA_DEBATE_COMMENT['countDown'] = int(data['countDown'])
+        DATA_DEBATE_COMMENT['currentCountDown'] = int(data['currentCountDown'])
+        return generate_json(DATA_DEBATE_COMMENT)
+    else:
+        DATA_DEBATE_COMMENT['img'] = get_comment_photo()
+        return generate_json(DATA_DEBATE_COMMENT)
+
+@app.route('/debate/data/comment/photo', methods=['GET', 'POST'])
+def debate_comment_photo():
+    global DATA_DEBATE_COMMENT
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            save_comment_photo(file)
+            return generate_json(DATA_DEBATE_COMMENT)
+    else:
+        DATA_DEBATE_COMMENT['img'] = get_comment_photo()
+        return generate_json(DATA_DEBATE_COMMENT)
 
 @app.route('/data')
 def data():
@@ -194,4 +350,5 @@ def generate_json(f):
 
 
 if __name__ == "__main__":
+    countDown()
     app.run(host='0.0.0.0', port=config.SERVER_PORT, threaded=True, debug=True, use_reloader=True)
